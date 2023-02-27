1101d66693097820390b769de2486b3d478f1cf3
MrGoropeza <goropeza8@gmail.com>
2023-02-27 00:10:28 -0300

    feat: dynamic pipe args optional

--- src/app/crud-table/utils/pipes/dynamic.pipe.ts
+++ src/app/crud-table/utils/pipes/dynamic.pipe.ts
@@ -7,13 +7,17 @@ import { Injector, Pipe, PipeTransform, Type } from "@angular/core";
 export class DynamicPipe implements PipeTransform {
   constructor(private injector: Injector) {}
 
-  transform(value: any, requiredPipe: Type<any>, args: any[]): any {
+  transform(value: any, requiredPipe: Type<any>, args?: any[]): any {
     const injector = Injector.create({
       name: "DynamicPipe",
       parent: this.injector,
       providers: [{ provide: requiredPipe }],
     });
     const pipe = injector.get(requiredPipe);
-    return pipe.transform(value, ...args);
+    if (args) {
+      return pipe.transform(value, ...args);
+    } else {
+      return pipe.transform(value);
+    }
   }
 }
