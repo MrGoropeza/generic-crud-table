7b0f07c469f578dd45571146bf1bcf2e079a1980
MrGoropeza <goropeza8@gmail.com>
2023-03-19 21:38:20 -0300

    feat: use of new crud table store

--- src/app/crud-table/crud-table.component.ts
+++ src/app/crud-table/crud-table.component.ts
@@ -1,10 +1,8 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
-import {
-  ConfirmationService,
-  LazyLoadEvent,
-  MessageService,
-} from "primeng/api";
+import { Component, Input, OnInit } from "@angular/core";
+import { LetModule } from "@ngrx/component";
+import { provideComponentStore } from "@ngrx/component-store";
+import { ConfirmationService, MessageService } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { ConfirmDialogModule } from "primeng/confirmdialog";
 import { DropdownModule } from "primeng/dropdown";
@@ -12,11 +10,10 @@ import { DialogService, DynamicDialogModule } from "primeng/dynamicdialog";
 import { InputTextModule } from "primeng/inputtext";
 import { TableModule } from "primeng/table";
 import { ToastModule } from "primeng/toast";
-import { map, Observable, tap } from "rxjs";
+import { CrudTableStore } from "./data-access/crud-table.store";
 import { CrudService } from "./data-access/crud.service";
 import { CrudTableBodyComponent } from "./ui/crud-table-body/crud-table-body.component";
 import { CrudTableHeaderComponent } from "./ui/crud-table-header/crud-table-header.component";
-import { CrudTableModalComponent } from "./ui/crud-table-modal/crud-table-modal.component";
 import { CrudTableModel } from "./utils/models/crud-table.model";
 import { DynamicPipe } from "./utils/pipes/dynamic.pipe";
 
@@ -37,84 +34,32 @@ import { DynamicPipe } from "./utils/pipes/dynamic.pipe";
     ToastModule,
     ConfirmDialogModule,
     DynamicDialogModule,
+    LetModule,
   ],
-  providers: [DialogService, MessageService, ConfirmationService],
+  providers: [
+    provideComponentStore(CrudTableStore),
+    DialogService,
+    MessageService,
+    ConfirmationService,
+  ],
 })
-export class CrudTableComponent<Model extends CrudTableModel> {
+export class CrudTableComponent<Model extends CrudTableModel>
+  implements OnInit
+{
   @Input() modelClass!: Model;
   @Input() service!: CrudService<Model>;
 
-  loading = false;
-  rows = 5;
-  values!: Observable<Model[]>;
-  totalRecords = 0;
+  constructor(protected store: CrudTableStore<Model>) {}
 
-  constructor(
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
-    private dialogService: DialogService
-  ) {}
+  ngOnInit(): void {
+    this.store.service = this.service;
+    this.store.modelClass = this.modelClass;
 
-  onLazyLoad(event: LazyLoadEvent) {
-    this.loading = true;
-    this.values = this.service.lazyList(event).pipe(
-      tap((response) => {
-        this.totalRecords = response.totalItems;
-      }),
-      map((response) => response.items),
-      tap(() => (this.loading = false))
-    );
-  }
-
-  newRow() {
-    this.dialogService.open(CrudTableModalComponent, {
-      closable: false,
-      closeOnEscape: false,
-      header: `Nueva ${this.modelClass.TableDefinition.singleRecordName}`,
-      data: { value: this.modelClass, service: this.service },
-    });
-  }
-
-  editRow(value: Model) {
-    this.dialogService.open(CrudTableModalComponent, {
-      header: `Editar "${value.Label}"`,
-      data: { value, service: this.service },
-    });
-  }
-
-  deleteRow(row: Model) {
-    this.confirmationService.confirm({
-      header: `Eliminar "${row.Label}"`,
-      message: "¿Seguro que querés eliminar este registro?",
-      acceptButtonStyleClass: "p-button-danger",
-      acceptLabel: "Sí",
-      rejectButtonStyleClass: "p-button-info",
-      rejectLabel: "No",
-      accept: () => {
-        this.loading = true;
-        this.service.delete(row.id).subscribe({
-          complete: () => {
-            this.messageService.add({
-              severity: "success",
-              summary: "Éxito",
-              detail: `"${row.Label}" eliminado con éxito`,
-            });
-            this.loading = false;
-          },
-          error: (e) => {
-            this.messageService.add({
-              severity: "error",
-              summary: "Error",
-              detail: "Error al borrar el registro, intente de nuevo.",
-            });
-            console.log(
-              `Error al borrar ${this.modelClass.TableDefinition.title}: `,
-              e
-            );
-            this.loading = false;
-          },
-        });
-      },
+    this.store.setState({
+      records: [],
+      totalRecords: 0,
+      rows: 5,
+      loading: false,
     });
   }
 }
